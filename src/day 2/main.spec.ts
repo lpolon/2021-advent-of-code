e90dec2ed074d31802e725e738668b7d7972e6a2
Leonardo Polon <leonardo.polon@b2wdigital.com>
2021-12-03 21:39:34 -0300

    day 2 done

--- src/day 2/main.spec.ts
+++ src/day 2/main.spec.ts
@@ -1,29 +1,69 @@
-import { parseInput } from './main';
+import fetchInput from 'src/helper/fetchInput';
+import {
+  getCoordinates,
+  getCoordinatesWithAim,
+  getSubCoordinatesProduct,
+  parseInput,
+} from './main';
 
-it('day 2 parse input should work as expected', () => {
-  const output = parseInput('forward 8\ndown 9\nup 4\ndown 8\ndown 3');
+const buildInputExample = () => 'forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2';
+
+test('day 2 parse input should work as expected', () => {
+  const input = buildInputExample();
+  const output = parseInput(input);
   expect(output).toMatchInlineSnapshot(`
 Array [
+  Object {
+    "direction": "forward",
+    "distance": 5,
+  },
+  Object {
+    "direction": "down",
+    "distance": 5,
+  },
   Object {
     "direction": "forward",
     "distance": 8,
   },
   Object {
-    "direction": "down",
-    "distance": 9,
-  },
-  Object {
     "direction": "up",
-    "distance": 4,
+    "distance": 3,
   },
   Object {
     "direction": "down",
     "distance": 8,
   },
   Object {
-    "direction": "down",
-    "distance": 3,
+    "direction": "forward",
+    "distance": 2,
   },
 ]
 `);
+});
+
+test('day 2-1 example input', () => {
+  const input = buildInputExample();
+  const output = getSubCoordinatesProduct(input, getCoordinates);
+  expect(output).toEqual((5 + 8 + 2) * (5 - 3 + 8));
+});
+
+test('day 2-1 input result', () => {
+  const input = fetchInput(2);
+  const output = getSubCoordinatesProduct(input, getCoordinates);
+
+  expect(output).toMatchInlineSnapshot(`1459206`);
+});
+
+test('day 2-2 example input', () => {
+  const input = buildInputExample();
+  const output = getSubCoordinatesProduct(input, getCoordinatesWithAim);
+
+  expect(output).toEqual(900);
+});
+
+test('day 2-1 input result', () => {
+  const input = fetchInput(2);
+  const output = getSubCoordinatesProduct(input, getCoordinatesWithAim);
+
+  expect(output).toMatchInlineSnapshot(`1320534480`);
 });
--- src/day 2/main.ts
+++ src/day 2/main.ts
@@ -1,15 +1,24 @@
 const subDirectionTup = ['forward', 'down', 'up'] as const;
-type SubDirection = typeof subDirectionTup[number];
-type SubInstruction = {
-  direction: SubDirection;
+
+type Direction = typeof subDirectionTup[number];
+
+type Command = {
+  direction: Direction;
   distance: number;
 };
 
-const isValidDirection = (d: string): d is SubDirection => {
+type SubCoordinates = {
+  hDir: number;
+  depth: number;
+};
+
+type SubCoordinatesWithAim = SubCoordinates & { aim: number };
+
+const isValidDirection = (d: string): d is Direction => {
   return subDirectionTup.includes(d);
 };
 
-export const parseInput = (input: string): SubInstruction[] =>
+export const parseInput = (input: string): Command[] =>
   input
     .split(/\n/)
     .map((i) => i.split(/\s/))
@@ -22,4 +31,55 @@ export const parseInput = (input: string): SubInstruction[] =>
         distance: Number(distance),
       };
     })
-    .filter((s: SubInstruction | undefined): s is SubInstruction => Boolean(s));
+    .filter((s: Command | undefined): s is Command => Boolean(s));
+
+export const getCoordinates = (input: string): SubCoordinates => {
+  return parseInput(input).reduce<SubCoordinates>(
+    (acc, { direction, distance }) => {
+      switch (direction) {
+        case 'up':
+          acc.depth -= distance;
+          return acc;
+        case 'down':
+          acc.depth += distance;
+          return acc;
+        case 'forward':
+          acc.hDir += distance;
+          return acc;
+      }
+    },
+    { hDir: 0, depth: 0 },
+  );
+};
+
+export const getCoordinatesWithAim = (input: string): SubCoordinatesWithAim => {
+  return parseInput(input).reduce<SubCoordinatesWithAim>(
+    (acc, { direction, distance }) => {
+      switch (direction) {
+        case 'up':
+          acc.aim -= distance;
+          break;
+        case 'down':
+          acc.aim += distance;
+          break;
+        case 'forward':
+          acc.hDir += distance;
+          acc.depth += acc.aim * distance;
+          break;
+      }
+      return acc;
+    },
+    { hDir: 0, depth: 0, aim: 0 },
+  );
+};
+
+export const getSubCoordinatesProduct = <
+  Coords extends { depth: number; hDir: number },
+  CoordsFn extends (input: string) => Coords,
+>(
+  input: string,
+  getCoordinatesFn: CoordsFn,
+): number => {
+  const { depth, hDir } = getCoordinatesFn(input);
+  return depth * hDir;
+};
