3bc00f10da4cfa9acd620bf0294b6a341cab5229
Leonardo Polon <leonardo.polon@b2wdigital.com>
2021-12-18 12:44:28 -0300

    wip: day 5 - 1

--- src/day 5/main.spec.ts
+++ src/day 5/main.spec.ts
@@ -1,27 +1,82 @@
 import fs from 'fs';
 import path from 'path';
-import { parseInput } from './main';
+import { buildDiagram, parseInput } from './main';
 
 const fetchExampleInput = () =>
   fs.readFileSync(path.resolve(__dirname, './example.input.txt'), 'utf-8');
 
 /*
-[
-      { x1: 0, y1: 9, x2: 5, y2: 9 },
-      { x1: 8, y1: 0, x2: 0, y2: 8 },
-      { x1: 9, y1: 4, x2: 3, y2: 4 },
-      { x1: 2, y1: 2, x2: 2, y2: 1 },
-      { x1: 7, y1: 0, x2: 7, y2: 4 },
-      { x1: 6, y1: 4, x2: 2, y2: 0 },
-      { x1: 0, y1: 9, x2: 2, y2: 9 },
-      { x1: 3, y1: 4, x2: 1, y2: 4 },
-      { x1: 0, y1: 0, x2: 8, y2: 8 },
-      { x1: 5, y1: 5, x2: 8, y2: 2 }
-    ]
+
 */
+
+const buildCoordsList = () => [
+  { x1: 0, y1: 5, x2: 5, y2: 5 },
+  { x1: 3, y1: 1, x2: 3, y2: 5 },
+  { x1: 5, y1: 4, x2: 3, y2: 4 },
+  { x1: 2, y1: 2, x2: 2, y2: 1 },
+];
 
 test('parse input works', () => {
   const input = fetchExampleInput();
   const output = parseInput(input);
   expect(output).toMatchSnapshot();
+});
+
+test('build diagram', () => {
+  const coordsList = buildCoordsList();
+  const output = buildDiagram(coordsList);
+  /*
+  - x ou y é constante.
+  - encontre o constante e acesse a linha ou coluna.
+  - itera sobre a diferença dos dois.. como?
+    - se for negativo. itera do maior pro menor
+    - se for positivo. do menor pro maior.
+  */
+  expect(output).toMatchInlineSnapshot(`
+[
+  [
+    ,
+    ,
+    0,
+  ],
+  [
+    ,
+    ,
+    0,
+    0,
+  ],
+  [
+    ,
+    ,
+    ,
+    0,
+  ],
+  [
+    ,
+    ,
+    ,
+    0,
+  ],
+  [
+    ,
+    ,
+    2,
+    0,
+  ],
+  [
+    ,
+    5,
+    ,
+    0,
+  ],
+]
+`);
+  // expect(output).toEqual([
+  //   [0, 0, 0, 0, 0, 0],
+  //   [0, 0, 1, 1, 0, 0],
+  //   [0, 0, 1, 1, 0, 0],
+  //   [0, 0, 0, 1, 0, 0],
+  //   [0, 0, 0, 2, 1, 1],
+  //   [1, 1, 1, 2, 1, 1],
+  // ]);
 });
--- src/day 5/main.ts
+++ src/day 5/main.ts
@@ -4,6 +4,13 @@ type Coords = {
   y1: number;
   y2: number;
 };
+/*
+Ajustes: iterar uma vez para pegar o maior valor de x e o maior de y.
+construir um array de tamanho x + 1;
+construir um array de x de tamanho y + 1;
+limpar lógica validando antes.
+Usar reduce & DI para as diferenças entre cenários.
+*/
 
 export const parseInput = (input: string): Coords[] => {
   const coords: Coords[] = [];
@@ -13,8 +20,87 @@ export const parseInput = (input: string): Coords[] => {
     if (!matchResult) {
       continue;
     }
-    const [,x1, y1, x2, y2] = matchResult.map(Number);
+    const [, x1, y1, x2, y2] = matchResult.map(Number);
     coords.push({ x1, y1, x2, y2 });
   }
   return coords;
+};
+
+export const buildDiagram = (coordsList: Coords[]): number[][] => {
+  const diagram: number[][] = [];
+
+  coordsList.forEach(({ x1, x2, y1, y2 }) => {
+    if (x1 === x2) {
+      const xIndex = x1;
+      const yDelta = y2 - y1;
+      if (yDelta > 0) {
+        const yInitial = y1;
+        for (let i = 0; i <= yDelta; i += 1) {
+          if (diagram[yInitial + i] === undefined) {
+            diagram[yInitial + i] = [];
+          }
+          const currentCoord = diagram[yInitial + i][xIndex];
+          if (currentCoord === undefined) {
+            diagram[yInitial + i][xIndex] = 0;
+          } else {
+            diagram[yInitial + i][xIndex] += 1;
+          }
+        }
+      } else {
+        const yInitial = y2;
+        for (let i = Math.abs(yDelta); i >= 0; i -= 1) {
+          if (diagram[yInitial - i] === undefined) {
+            diagram[yInitial - i] = [];
+          }
+          const currentCoord = diagram[yInitial - i][xIndex];
+          if (currentCoord === undefined) {
+            diagram[yInitial - i][xIndex] = 0;
+          } else {
+            diagram[yInitial - i][xIndex] += 1;
+          }
+        }
+      }
+    } else {
+      const yIndex = y1;
+      const xDelta = x2 - x1;
+      if (xDelta > 0) {
+        const xInitial = x1;
+        for (let i = 0; i <= xDelta; i += 1) {
+          if (diagram[yIndex] === undefined) {
+            diagram[yIndex] = [];
+          }
+          const currentCoord = diagram[yIndex][xInitial + 1];
+          if (currentCoord === undefined) {
+            diagram[yIndex][xInitial + 1] = 0;
+          } else {
+            diagram[yIndex][xInitial + 1] += 1;
+          }
+        }
+      } else {
+        const xInitial = x2;
+        for (let i = Math.abs(xDelta); i >= 0; i -= 1) {
+          if (diagram[yIndex] === undefined) {
+            diagram[yIndex] = [];
+          }
+          const currentCoord = diagram[yIndex][xInitial - 1];
+          if (currentCoord === undefined) {
+            diagram[yIndex][xInitial - 1] = 0;
+          } else {
+            diagram[yIndex][xInitial - 1] += 1;
+          }
+        }
+      }
+    }
+  });
+  console.log('antes', diagram);
+  diagram.forEach((row, i) => {
+    row.forEach((value, j) => {
+      console.log('qual foi', value);
+      if (diagram[i][j] === undefined) {
+        diagram[i][j] = 0;
+      }
+    });
+  });
+  console.log('depois', diagram);
+  return diagram;
 };
